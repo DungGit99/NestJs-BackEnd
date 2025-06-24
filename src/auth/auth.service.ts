a38583e1347097dc91f410e4eb733c2e88a4025c
Dũng Tịnh Tâm <48619198+DungGit99@users.noreply.github.com>
2025-06-24 16:49:09 +0700

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,32 +1,43 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { comparePasswordHelper } from 'src/helpers/util';
 import { User } from 'src/users/schemas/user.schema';
-
+import { v4 as uuidv4 } from 'uuid';
+import * as dayjs from 'dayjs';
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
-    private jwtService: JwtService) { }
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, password: string): Promise<User | null> {
-    const user = await this.usersService.findByUsername(username)
-    const matchPassword = await comparePasswordHelper(password, user.password)
-    if(!user || !matchPassword) {
-      return null
+    const user = await this.usersService.findByUsername(username);
+    const matchPassword = await comparePasswordHelper(password, user.password);
+    if (!user || !matchPassword) {
+      return null;
     }
-    return user
+    return user;
   }
-  async login(user:User): Promise<any> {
-    const payload = { username: user.username, role: user.role }
-    const token = await this.jwtService.signAsync(payload)
+  async login(user: User): Promise<any> {
+    const payload = { username: user.username, role: user.role };
+    const token = await this.jwtService.signAsync(payload);
     return {
       access_token: token,
       userInfo: {
         username: user.username,
         role: user.role,
-      }
-    }
+      },
+    };
+  }
+
+  async forgotPassword(email: string): Promise<{ message: string }> {
+    const user = await this.usersService.findByEmail(email);
+    if (!user) throw new NotFoundException();
+    user.codeId = uuidv4();
+    user.codeExpired = dayjs().add(15, 'minutes').toDate();
+    await user.save();
+    return { message: 'Mật khẩu đã được đặt lại thành công.' };
   }
 }
