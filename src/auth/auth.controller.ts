da8434c1b9ec07e19c50537c2993a13d678097dd
DungGit99 <nguyendungcode99@gmail.com>
2025-06-25 09:07:54 +0700

    dungnv

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './passport/local-auth.guard';
@@ -8,23 +15,23 @@ import { ForgotPasswordDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(private readonly authService: AuthService) {}
 
   @Post('login')
   @Public() // Đánh dấu route này là công khai SetMetadata
   @UseGuards(LocalAuthGuard)
   async login(@Request() req) {
-    return this.authService.login(req.user)
+    return this.authService.login(req.user);
   }
   // Bearer token
   // @UseGuards(JwtAuthGuard)
   @Get('profile')
-  getProfile(@Request() req){
-    return req.user
+  getProfile(@Request() req) {
+    return req.user;
   }
   @Public()
   @Post('forgot-password')
   async forgotPassword(@Body() forgotPasswordDto: ForgotPasswordDto) {
     return await this.authService.forgotPassword(forgotPasswordDto.email);
   }
-} 
+}
